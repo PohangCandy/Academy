f763ec7a063b740417ce5ca263620953ffa2dfc4
PohangCandy <rlcks777@naver.com>
2025-09-08 11:54:17 +0900

    making Astar with gui and making set des,start pos with bitmask

---
+++ course2/AStar/AStarClass/AStar/AStar.cpp
@@ -0,0 +1,117 @@
+#include "Astar.h"
+#include <iostream>
+using namespace std;
+
+//일단 제일 처음 좌표에서 도착지까지 H는 계산되야 함.
+//뉴클리드 계산
+float AStar::findGbyNode(Node* s, Node* d)
+{
+	//대각선으로 이동한 경우
+	if (abs(s->pos.x - d->pos.x) + abs(s->pos.y - d->pos.y) == 2) return s->G + 1.4;
+	//직선으로 이동한 경우
+	else return s->G + 1;
+}
+
+float AStar::findF(Node* n)
+{
+	return n->H + n->G;
+}
+
+//도착지가 맴버로 저장되어있으므로 출발지만 갱신하면서 재귀해주면 될 것으로 보임.
+//탐색한 경로를 리스트에 담으면서 f가 가장 적은 곳을 먼저 탐색하도록 만든다.
+void AStar::insertListEightDirection(Node* sn)
+{
+	int dx[8] = { 0,1,1,1,0,-1,-1,-1 };
+	int dy[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+	for (int i = 0; i < 8; i++)
+	{
+		int nx = sn->pos.x + dx[i];
+		int ny = sn->pos.y + dy[i];
+		if (nx < 0 || ny < 0 || nx >= width || ny >= length) continue;
+
+		Node* newNode = new Node;
+		newNode->parent = sn;
+		newNode->pos.x = nx;
+		newNode->pos.y = ny;
+		newNode->G = findGbyNode(sn, newNode);
+		newNode->H = findHbyGrid(&newNode->pos);
+		newNode->F = findF(newNode);
+
+		//방문한 노드는 다시 방문하지 않도록 해준다.
+		//우선순위 큐라 값을 찾지 못한다.
+		bool visited = false;
+
+		for (auto& a : _closelist)
+		{
+			if (newNode->pos == a->pos)
+			{
+				visited = true;
+				break;
+			}
+		}
+		if (visited)
+		{
+			delete newNode;
+			continue;
+		}
+
+		//openlist에도 이미 방문중인 노드일 수 있음.
+		visited = false;
+		for (auto& a : _openlist)
+		{
+			if (newNode->pos == a->pos)
+			{
+				//만약 새로운 경로의 F가 더 적다면 갱신해준다.
+				if (newNode->F < a->F)
+				{
+					a->G = newNode->G;
+					a->H = newNode->H;
+					a->F = newNode->F;
+					a->parent = newNode->parent;
+				}
+				visited = true;
+				break;
+			}
+		}
+		if (visited)
+		{
+			delete newNode;
+			continue;
+		}
+
+
+
+		_openlist.insert(newNode);
+	}
+}
+
+bool AStar::findPath()
+{
+	while (!_openlist.empty())
+	{
+		auto bestIt = _openlist.begin();
+		Node* top = *bestIt;
+		_openlist.erase(bestIt);
+		//갔던 곳 다시 가지 않도록 표시
+		_closelist.push_back(top);
+
+		//현재 방문한 노드 출력
+		cout << "[x pos] : " << top->pos.x << " [y pos] : " << top->pos.y << "\n";
+		cout << " [G] : " << top->G << " [H] : " << top->H << " [F] : " << top->F << "\n";
+		//최종 목적지에 도달했다면 중단
+		if (top->pos == _destination)
+		{
+			cout << "목적지에 도달했습니다." << "\n";
+			return true;
+		}
+		insertListEightDirection(top);
+	}
+	return false;
+}
+
+//일단 제일 처음 좌표에서 도착지까지 H는 계산되야 함.
+float AStar::findHbyGrid(Grid* s)
+{
+	return abs(s->x - _destination.x) + abs(s->y - _destination.y);
+}
